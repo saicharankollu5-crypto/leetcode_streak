8d400b405f953bd43f85421c13c9a65405f93ebf
saicharankollu5-crypto <saicharankollu5@gmail.com>
2026-09-17 10:25:55 +0530

    Time: 1 ms (96.65%) | Memory: 42.5 MB (73.59%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+      while (n>0 & n%2==0){
+        n = n/2;
+      }  
+      if (n==1){
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+}
